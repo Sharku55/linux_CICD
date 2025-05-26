3ef7f89b45dad09a60ee7dfab4cea19673465a12
Sharku55 <98410200+Sharku55@users.noreply.github.com>
2025-05-26 23:43:12 +0300

    Update notify.sh

--- src/notify.sh
+++ src/notify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TELEGRAM_API_TOKEN="6997617271:AAEo5NKIybDVdE2sywpNH5nQ0-8YUjVE8Wk"
-CHAT_ID="1771558961"
+TELEGRAM_API_TOKEN="**************"
+CHAT_ID="********"
 
 URL="https://api.telegram.org/bot$TELEGRAM_API_TOKEN/sendMessage"
 TEXT="Deploy status: $1%0AProject name: $CI_PROJECT_NAME%0AURL: $CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/"
